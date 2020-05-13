f8789e24c26e48d3445b86496aa1dd3323f59e26
daantoma <daantoma@hotmail.com>
2020-05-13 00:49:37 -0500

    se retiran los inicios provisionales

--- Hotel/ClientApp/src/app/app-routing.module.ts
+++ Hotel/ClientApp/src/app/app-routing.module.ts
@@ -7,17 +7,13 @@ import { ClienteEditaComponent } from './componentes/cliente/cliente-edita/clien
 import { RecepcionistaRegistroComponent } from './componentes/recepcionista/recepcionista-registro/recepcionista-registro.component';
 import { RecepcionistaConsultaComponent } from './componentes/recepcionista/recepcionista-consulta/recepcionista-consulta.component';
 import { RecepcionistaEditaComponent } from './componentes/recepcionista/recepcionista-edita/recepcionista-edita.component';
-import { RecepcionistaInicioProvicionalComponent } from './componentes/recepcionista/recepcionista-inicio-provicional/recepcionista-inicio-provicional.component';
 import { ProductoEditaComponent } from './componentes/producto/producto-edita/producto-edita.component';
-import { ProductoInicioProvicionalComponent } from './componentes/producto/producto-inicio-provicional/producto-inicio-provicional.component';
 import { ProductoConsultaComponent } from './componentes/producto/producto-consulta/producto-consulta.component';
 import { ProductoRegistroComponent } from './componentes/producto/producto-registro/producto-registro.component';
 import { HabitacionRegistroComponent } from './componentes/habitacion/habitacion-registro/habitacion-registro.component';
 import { HabitacionConsultaComponent } from './componentes/habitacion/habitacion-consulta/habitacion-consulta.component';
 import { ReservaRegistroComponent } from './componentes/reserva/reserva-registro/reserva-registro.component';
 import { HomeComponent } from './home/home.component';
-import { HabitacionInicioProvicionalComponent } from './componentes/habitacion/habitacion-inicio-provicional/habitacion-inicio-provicional.component';
-import { ReservaInicioProvicionalComponent } from './componentes/reserva/reserva-inicio-provicional/reserva-inicio-provicional.component';
 import { HabitacionEditaComponent } from './componentes/habitacion/habitacion-edita/habitacion-edita.component';
 import { ReservaConsultaComponent } from './componentes/reserva/reserva-consulta/reserva-consulta.component';
 import { ReservaEditaComponent } from './componentes/reserva/reserva-edita/reserva-edita.component';
@@ -45,10 +41,6 @@ const routes: Routes = [
 
   // recepcionista--------------------------------------------------------------------------------------
   {
-    path: 'recepcionistaInicioProvisional',
-    component: RecepcionistaInicioProvicionalComponent
-  },
-  {
     path: 'recepcionistaConsulta',
     component: RecepcionistaConsultaComponent 
   },
@@ -66,10 +58,6 @@ const routes: Routes = [
   {
     path: 'productoEdita/:id',
     component: ProductoEditaComponent 
-  },
-  {
-    path: 'productoInicioProvisional',
-    component: ProductoInicioProvicionalComponent 
   },
   {
     path: 'productoConsulta',
@@ -94,20 +82,12 @@ const routes: Routes = [
     path: 'habitacionEdita/:id',
     component: HabitacionEditaComponent
   },
-  {
-    path: 'habitacionInicioProvisional',
-    component: HabitacionInicioProvicionalComponent 
-  },
 
   // reserva--------------------------------------------------------------------------------------------
 
   {
     path: 'reservaRegistro',
     component: ReservaRegistroComponent 
-  },
-  {
-    path: 'reservaInicioProvisional',
-    component: ReservaInicioProvicionalComponent 
   },
   {
     path: 'reservaConsulta',
--- Hotel/ClientApp/src/app/app.module.ts
+++ Hotel/ClientApp/src/app/app.module.ts
@@ -14,11 +14,9 @@ import { ClienteEditaComponent } from './componentes/cliente/cliente-edita/clien
 import { ProductoConsultaComponent } from './componentes/producto/producto-consulta/producto-consulta.component';
 import { ProductoRegistroComponent } from './componentes/producto/producto-registro/producto-registro.component';
 import { ProductoEditaComponent } from './componentes/producto/producto-edita/producto-edita.component';
-import { ProductoInicioProvicionalComponent } from './componentes/producto/producto-inicio-provicional/producto-inicio-provicional.component';
 import { ReservaConsultaComponent } from './componentes/reserva/reserva-consulta/reserva-consulta.component';
 import { ReservaRegistroComponent } from './componentes/reserva/reserva-registro/reserva-registro.component';
 import { ReservaEditaComponent } from './componentes/reserva/reserva-edita/reserva-edita.component';
-import { ReservaInicioProvicionalComponent } from './componentes/reserva/reserva-inicio-provicional/reserva-inicio-provicional.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AlertModalComponent } from './@base/alert-modal/alert-modal.component';
 import { FiltroClientePipe } from './pipe/filtro-cliente.pipe';
@@ -29,11 +27,9 @@ import { FiltroReservaPipe } from './pipe/filtro-reserva.pipe';
 import { RecepcionistaConsultaComponent } from './componentes/recepcionista/recepcionista-consulta/recepcionista-consulta.component';
 import { RecepcionistaRegistroComponent } from './componentes/recepcionista/recepcionista-registro/recepcionista-registro.component';
 import { RecepcionistaEditaComponent } from './componentes/recepcionista/recepcionista-edita/recepcionista-edita.component';
-import { RecepcionistaInicioProvicionalComponent } from './componentes/recepcionista/recepcionista-inicio-provicional/recepcionista-inicio-provicional.component';
 import { HabitacionConsultaComponent } from './componentes/habitacion/habitacion-consulta/habitacion-consulta.component';
 import { HabitacionRegistroComponent } from './componentes/habitacion/habitacion-registro/habitacion-registro.component';
 import { HabitacionEditaComponent } from './componentes/habitacion/habitacion-edita/habitacion-edita.component';
-import { HabitacionInicioProvicionalComponent } from './componentes/habitacion/habitacion-inicio-provicional/habitacion-inicio-provicional.component';
 import { BannerAdminComponent } from './componentes/banners/banner-admin/banner-admin.component';
 import { BannerClienteComponent } from './componentes/banners/banner-cliente/banner-cliente.component';
 import { BannerRecepcionistaComponent } from './componentes/banners/banner-recepcionista/banner-recepcionista.component';
@@ -51,19 +47,15 @@ import { LoginComponent } from './componentes/login/login.component';
     RecepcionistaConsultaComponent,
     RecepcionistaRegistroComponent,
     RecepcionistaEditaComponent,
-    RecepcionistaInicioProvicionalComponent,
     HabitacionConsultaComponent,
     HabitacionRegistroComponent,
     HabitacionEditaComponent,
-    HabitacionInicioProvicionalComponent,
     ProductoConsultaComponent,
     ProductoRegistroComponent,
     ProductoEditaComponent,
-    ProductoInicioProvicionalComponent,
     ReservaConsultaComponent,
     ReservaRegistroComponent,
     ReservaEditaComponent,
-    ReservaInicioProvicionalComponent,
     AlertModalComponent,
     FiltroClientePipe,
     FiltroRecepcionistaPipe,
