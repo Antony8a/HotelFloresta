b4ac4a681454d3a570f3030c8c0d6f399b53323e
Daniel Andrés Torres Mahecha <56619256+daantoma@users.noreply.github.com>
2020-06-04 22:14:57 -0500

    cambiando ruta de acceso a la bd

--- Hotel/Controllers/ClienteController.cs
+++ Hotel/Controllers/ClienteController.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Hotel.Models;
+using Datos;
 
 namespace Hotel.Controllers
 {
@@ -15,14 +16,11 @@ namespace Hotel.Controllers
     [ApiController]
     public class ClienteController : ControllerBase
     {
-        
         private readonly ClienteService _clienteService;
-        public IConfiguration Configuration { get; }
-        public ClienteController(IConfiguration configuration)
+
+        public ClienteController(HotelContext context)
         {
-            Configuration = configuration;
-            string connectionString = Configuration["ConnectionStrings:DefaultConnection"];
-            _clienteService = new ClienteService(connectionString);
+            _clienteService = new ClienteService(context);
         }
         // GET: api/Cliente
         [HttpGet]
